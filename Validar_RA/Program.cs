c7561b12d0951ebb89770624b5a25516b853742f
Giovanna Dimas <128722193+GiovannaDimas@users.noreply.github.com>
2023-09-03 13:31:47 -0300

    Add files via upload

---
+++ Validar_RA/Program.cs
@@ -0,0 +1,62 @@
+﻿class Aluno
+{
+    protected string nome;
+    private string ra;
+
+    public Aluno(string nome, string ra)
+    {
+        this.nome = nome;
+        this.ra = ra;
+    }
+
+    public void ValidarRA(string ra)
+    {
+        if (this.ra.Equals(ra))
+        {
+            Console.WriteLine("Nome do aluno: " + nome);
+        }
+        else
+        {
+            Console.WriteLine("Esta matrícula não corresponde ao aluno.");
+        }
+    }
+}
+
+class AlunoValidacao : Aluno
+{
+    private DateTime dataValidacao;
+
+    public AlunoValidacao(string nome, string ra) : base(nome, ra)
+    {
+        dataValidacao = DateTime.Now;
+    }
+
+    public void MostrarDataValidacao()
+    {
+        Console.WriteLine("Data de validação: " + dataValidacao);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AlunoValidacao aluno = new AlunoValidacao("Giovanna Dimas", "N782587");
+
+        Console.Write("Digite o RA: ");
+        string raDigitado = Console.ReadLine();
+
+        
+        if (raDigitado != "")
+        {
+            aluno.ValidarRA(raDigitado);
+            aluno.MostrarDataValidacao();
+        }
+        else
+        {
+            Console.WriteLine("RA não digitado.");
+        }
+    }
+}
+
+
